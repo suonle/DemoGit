72f987f30bb329ec4c81b442a0300cb09d28062f
suonle <suonlp.21da@vku.udn.vn>
2023-10-18 14:20:14 +0700

    lap 4

---
+++ Midterm/app/src/main/java/Model/Note.kt
@@ -0,0 +1,18 @@
+package Model
+
+import android.text.Editable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+
+data class Note {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "content")
+    val content: String
+
+}
